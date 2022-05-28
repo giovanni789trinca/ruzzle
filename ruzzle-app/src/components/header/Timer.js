16a4810e09c6973d7887f7c346ddaaf772419637
Giovanni Trinca <100695608+giovanni789trinca@users.noreply.github.com>
2022-05-28 23:46:19 +0200

    timer

--- ruzzle-app/src/components/header/Timer.js
+++ ruzzle-app/src/components/header/Timer.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { BiTimer } from "react-icons/bi";
 import styles from "./Timer.module.css";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
 
 function Timer() {
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <p>Time's up!</p>;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          0{minutes}:{seconds}
+        </span>
+      );
+    }
+  };
   return (
     <div className={styles.timer}>
       <BiTimer />
-      <p className={styles.timer__decrease}>1:12</p>
+      <p className={styles.timer__decrease}>
+        <Countdown date={Date.now() + 60000} renderer={renderer} />
+        {/* <Countdown date={moment(Date.now() + 60000).format("mm:ss a")} /> */}
+      </p>
     </div>
   );
 }

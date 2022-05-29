58ef0223e86e9f81b019fce474c9c70c183902d7
Giovanni Trinca <100695608+giovanni789trinca@users.noreply.github.com>
2022-05-29 14:33:13 +0200

    end

--- ruzzle-app/src/components/UI/Button.js
+++ ruzzle-app/src/components/UI/Button.js
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from "./Button.module.css";
+import { useHistory } from "react-router-dom";
 
 function Button(props) {
+  const history = useHistory();
   const classes = styles.btn + " " + styles[props.className];
-  return <button className={classes}>{props.children}</button>;
+  const endGame = () => {
+    history.push("/");
+  };
+  return (
+    <button className={classes} onClick={endGame}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
